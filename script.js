90be21dfd780aecaada470f3e07d9d684d042dc6
oneofthenemos <warmachine646@gmail.com>
2021-10-02 20:27:54 +0530

    :tada: Added hand animation and code optimization

--- script.js
+++ script.js
@@ -2,40 +2,37 @@ const stonePaperScissors = ["Stone", "Paper", "Scissors"];
 const BtnStone = document.querySelector("#Stone");
 const BtnPaper = document.querySelector("#Paper");
 const BtnScissor = document.querySelector("#Scissor");
+const userHand = document.querySelector("#userSelect");
+const cpuHand = document.querySelector("#cpuSelect");
+const user = document.querySelector("#userSelection");
+const cpu = document.querySelector("#cpuSelection");
+const btns = document.querySelectorAll(".btn-play");
+
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 2.9);
+}
 
 function start(userInput) {
+  disableBtns();
   const userChoice = stonePaperScissors[userInput];
   const cpuChoice = stonePaperScissors[generateRandomNumber()];
   console.log(userChoice, cpuChoice);
   displaySelection(userChoice, cpuChoice);
 
   const didUserWon = chooseWinner(userChoice, cpuChoice);
-
-  let winner;
-  if (didUserWon) winner = "You";
-  else if (didUserWon === null) winner = null;
-  else winner = "AI";
-  displayWinner(winner);
 }
 
 function displaySelection(userSelection, cpuSelection) {
   console.log(userSelection, cpuSelection);
-  const user = document.querySelector("#userSelection");
-  const cpu = document.querySelector("#cpuSelection");
+
+  userHand.innerHTML = "✊";
+  cpuHand.innerHTML = "✊";
+  userHand.classList.add("animateFistOne");
+  cpuHand.classList.add("animateFistTwo");
 
   user.innerHTML = userSelection;
   cpu.innerHTML = cpuSelection;
-}
-
-function displayWinner(winner) {
-  const winnerText = document.querySelector("#result-text");
-
-  if (winner) winnerText.innerHTML = `${winner} Won!!`;
-  else winnerText.innerHTML = "It's a Tie 😶";
-}
-
-function generateRandomNumber() {
-  return Math.floor(Math.random() * 2.9);
+  setTimeout(endGameRituals, 2000);
 }
 
 function chooseWinner(user, cpu) {
@@ -59,4 +56,56 @@ function chooseWinner(user, cpu) {
       return false;
     }
   }
+}
+
+function displayWinner(winner) {
+  const winnerText = document.querySelector("#result-text");
+
+  if (winner) winnerText.innerHTML = `${winner} Won!!`;
+  else winnerText.innerHTML = "It's a Tie 😶";
+}
+
+function changeUserHandGestures(x, y) {
+  console.log(x, y);
+  switch (x) {
+    case "Scissors":
+      userHand.innerHTML = "🖖";
+      break;
+    case "Paper":
+      userHand.innerHTML = "✋";
+      break;
+  }
+  switch (y) {
+    case "Scissors":
+      cpuHand.innerHTML = "🖖";
+      break;
+    case "Paper":
+      cpuHand.innerHTML = "✋";
+      break;
+  }
+}
+
+function endGameRituals() {
+  changeUserHandGestures(userSelection, cpuSelection);
+
+  const didUserWon = chooseWinner(userSelection, cpuSelection);
+  if (didUserWon) displayWinner("You");
+  else if (didUserWon === null) displayWinner(null);
+  else displayWinner("AI");
+
+  userHand.classList.remove("animateFistOne");
+  cpuHand.classList.remove("animateFistTwo");
+  enableBtns();
+}
+
+function disableBtns() {
+  btns.forEach((btn) => {
+    btn.setAttribute("disabled", true);
+  });
+}
+
+function enableBtns() {
+  btns.forEach((btn) => {
+    btn.removeAttribute("disabled");
+  });
 }
